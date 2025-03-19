a683a97a06981f30a3ef7534500b3a15dc2eacd8
Suyog <np03cs4a230061@heraldcollege.edu.np>
2025-03-19 18:49:06 +0545

    Setup Node.js backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,19 @@
-console.log("Node js is Running!");
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config(); // Load environment variables
+
+const app = express();
+
+app.use(express.json()); // Middleware for JSON parsing
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("YatruSewa Backend is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
