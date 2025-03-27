285cb53bb31ce16327caa79481bc407ad4a0f626
Pralab <Np03cs4s230255@heraldcollege.edu.np>
2025-03-27 13:47:30 +0545

    Update Home page, Contact page and About page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,62 @@
-import React from 'react'
+"use client"
 
-const App = () => {
-    return (
-        <div>
-            <h1>Hello world</h1>
-        </div>
-    )
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { AuthProvider } from "./contexts/AuthContext"
+import { useEffect } from "react"
+import AOS from "aos"
+import "aos/dist/aos.css"
+
+// Auth Pages
+import SignIn from "./pages/auth/SignIn"
+import SignUp from "./pages/auth/SignUp"
+import ForgotPassword from "./pages/auth/ForgotPassword"
+import TwoStepVerification from "./pages/auth/TwoStepVerification"
+import SuccessPage from "./pages/auth/SuccessPage"
+
+// Static Pages
+import HomePage from "./pages/HomePage"
+import AboutPage from "./pages/AboutPage"
+import ContactPage from "./pages/ContactPage"
+
+// Layout Components
+import AuthLayout from "./layouts/AuthLayout"
+import MainLayout from "./layouts/MainLayout"
+
+function App() {
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-out-cubic",
+      once: true,
+    })
+  }, [])
+
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Auth Routes */}
+          <Route element={<AuthLayout />}>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/verify" element={<TwoStepVerification />} />
+            <Route path="/success" element={<SuccessPage />} />
+          </Route>
+
+          {/* Main Routes */}
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/contact" element={<ContactPage />} />
+          </Route>
+
+          {/* Default route redirects to home */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  )
 }
+
 export default App
