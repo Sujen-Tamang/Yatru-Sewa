6e0889539edaa461ecc53edfdf08a1ac0a1017e7
Suyog <np03cs4a230061@heraldcollege.edu.np>
2025-04-28 17:22:58 +0545

    login and signup fix

--- Backend/app.js
+++ Backend/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { connection } from "./database/dbConnection.js";
 import { errorMiddleware } from "./middlewares/error.js";
-import userRouter from "./routes/userRouter.js";
+import userRoutes from "./routes/userRouter.js";
 import { removeUnverifiedAccounts } from "./automation/removeUnverifiedAccounts.js";
 
 export const app = express();
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/v1/user", userRouter);
+app.use("/api/v1/user", userRoutes);
 
 removeUnverifiedAccounts();
 connection();
